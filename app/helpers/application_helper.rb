35d9b35be3792f71231f84983f85ceffb62a5cda
Vladimir Chervanev <vladimir.chervanev@traveltripper.com>
2020-03-27 17:18:28 -0500

    added pretty json output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def safe_render_json(content)
+    return '<empty>' if content.blank?
+
+    json = JSON.parse(content)
+    JSON.pretty_generate(json)
+  rescue JSON::ParserError
+    content
+  end
 end
